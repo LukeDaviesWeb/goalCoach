f0c682adfa82fc557e31e8b0dad8b5a90f66edcb
Luke Davies <lukedaviesweb@gmail.com>
2019-02-27 19:39:20 +0800

    update

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
-const config ={
-    apiKey: "AIzaSyB8SzqN8FnvjpjWT8iDD3QSU6Sr4lDVSZc",
-    authDomain: "goalcoach-bf10d.firebaseapp.com",
-    databaseURL: "https://goalcoach-bf10d.firebaseio.com",
-    projectId: "goalcoach-bf10d",
-    storageBucket: "goalcoach-bf10d.appspot.com",
-    messagingSenderId: "12677533113"
-  };
+const config = {
+  apiKey: 'API_KEY',
+  authDomain: 'DOMAIN',
+  databaseURL: 'DATABASE_URL',
+  projectId: 'PROJECT_ID',
+  storageBucket: 'STORAGE_BUCKET',
+  messagingSenderId: 'MESSAGING SENDER ID'
+};
 
-  export const firebaseApp = firebase.initializeApp(config);
-  export const goalRef = firebase.database().ref('goals');
-  export const completeGoalRef = firebase.database().ref('completeGoals');
+export const firebaseApp = firebase.initializeApp(config);
+export const goalRef = firebase.database().ref('goals');
+export const completeGoalRef = firebase.database().ref('completeGoals');
